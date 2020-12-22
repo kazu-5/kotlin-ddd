141dafe4015b63d0a4d4c62db86c12e152d93d15
Kazuhiro Yoshimoto <kazuhiro.yoshimoto@livesense.co.jp>
2020-12-22 18:39:52 +0900

    fix sql

--- src/main/resources/db/migration/V1.0.0__create_table.sql
+++ src/main/resources/db/migration/V1.0.0__create_table.sql
@@ -1,4 +1,3 @@
--- ユーザテーブル作成
 CREATE TABLE IF NOT EXISTS users
 (
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
@@ -8,7 +7,19 @@ CREATE TABLE IF NOT EXISTS users
     created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_on DATETIME ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
-    ) ENGINE=InnoDB;
+    );
 
--- ユーザデータ初期化
-INSERT INTO users (name, password) VALUES ("もりもり", "password");
+INSERT INTO users (name, password) VALUES ("user1", "password");
+
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    user_id MEDIUMINT NOT NULL,
+    foreign key fk_users (user_id) references users(id) on delete cascade on update cascade,
+    created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_on DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO tasks(name, user_id) VALUES ("task1", 1);
