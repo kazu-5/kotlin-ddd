945ee6a490b93dfddcc07393d15ec1f8cefafc3f
Kazuhiro Yoshimoto <kazuhiro.yoshimoto@livesense.co.jp>
2020-12-22 11:11:46 +0900

    first commit

---
+++ src/main/kotlin/controller/TestController.kt
@@ -0,0 +1,17 @@
+package controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class TestController {
+
+    @GetMapping("/qiitas")
+    fun index(): String {
+        println("test456")
+        return ""
+    }
+
+}
